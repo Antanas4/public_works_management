8decd3fbb0d6a9d8cc78b5223efb0538d235c475
Antanas <antanas.buteliauskas@gmail.com>
2026-04-16 22:45:36 +0300

    Adding types and cpv's

--- client/src/app/core/constants/case-types.constant.ts
+++ client/src/app/core/constants/case-types.constant.ts
@@ -5,7 +5,12 @@ export const CASE_TYPES: CaseType[] = [
         label: "Aplinkos tvarkymas",
         value: "ENVIRONMENT",
         subtypes: [
-            { label: "Komunalinių atliekų tvarkymas", value: "WASTE_MANAGEMENT" },
+            { label: "Kapinių priežiūra", value: "CEMETERY_MAINTENANCE" },
+            { label: "Komunalinių atliekų tvarkymas, konteineriai", value: "WASTE_MANAGEMENT" },
+            { label: "Želdynų priežiūra", value: "GREEN_AREA_MAINTENANCE" },
+            { label: "Vaikų žaidimo aikštelės", value: "PLAYGROUND_MAINTENANCE" },
+            { label: "Teritorijų tvarkymas", value: "TERRITORY_CLEANING" },
+            { label: "Sporto aikštelės, treniruokliai", value: "SPORT_FIELD_MAINTENANCE" },
         ]
     },
     {
@@ -13,6 +18,13 @@ export const CASE_TYPES: CaseType[] = [
         value: "SURFACE_REPAIR",
         subtypes: [
             { label: "Žvyrkeliai", value: "GRAVEL_ROAD" },
+            { label: "Laiptai", value: "STAIRCASE" },
+            { label: "Šaligatvių remontas", value: "SIDEWALK_REPAIR" },
+            { label: "Gatvių remontas", value: "ROAD_REPAIR" },
+            { label: "Kiemų dangos remontas", value: "YARD_SURFACE_REPAIR" },
+            { label: "Duobės, įgriuvos, šuliniai", value: "POTHOLES_WELLS_COLLAPSE" },
+            { label: "Dviračių takų problemos", value: "BIKE_PATH_REPAIR" },
+            { label: "Tiltų, viadukų, tunelių ir estakadų priežiūra", value: "BRIDGE_TUNNEL_MAINTENANCE" }
         ]
     },
     {
@@ -20,6 +32,36 @@ export const CASE_TYPES: CaseType[] = [
         value: "MALFUNCTION",
         subtypes: [
             { label: "Apšvietimo gedimai", value: "LIGHTING_FAILURES" },
+        ]
+    },
+    {
+        label: "Pastatai",
+        value: "BUILDINGS",
+        subtypes: [
+            { label: "Pastatų administravimas", value: "BUILDING_ADMINISTRATION" },
+            { label: "Statinių priežiūra", value: "BUILDING_MAINTENANCE" }
+        ]
+    },
+    {
+        label: "Sezoninės problemos",
+        value: "SEASONAL_ISSUES",
+        subtypes: [
+            { label: "Šaligatvių valymas", value: "SIDEWALK_CLEANING" },
+            { label: "Stotelių valymas", value: "BUS_STOP_CLEANING" },
+            { label: "Daugiabučių namų kiemų valymas", value: "YARD_CLEANING" },
+            { label: "Dviračių takų valymas", value: "BIKE_PATH_CLEANING" },
+            { label: "Automobilių stovėjimo aikštelių valymas", value: "PARKING_LOT_CLEANING" },
+            { label: "Gatvių valymas", value: "STREET_CLEANING" }
+        ]
+    },
+    {
+        label: "Eismas",
+        value: "TRAFFIC",
+        subtypes: [
+            { label: "Gatvių apšvietimas", value: "STREET_LIGHTING" },
+            { label: "Ženklai, horizontalus ženklinimas, greičio mažinimo kalneliai ir kitos eismo organizavimo priemonės", value: "TRAFFIC_SIGNS_AND_MARKING" },
+            { label: "Viešasis transportas", value: "PUBLIC_TRANSPORT_INFRASTRUCTURE" },
+            { label: "Šviesoforai (gedimai, sutrikimai)", value: "TRAFFIC_LIGHTS_FAILURE" }
         ]
     }
 ];
--- handler/src/main/java/org/handler/service/impl/CpvServiceImpl.java
+++ handler/src/main/java/org/handler/service/impl/CpvServiceImpl.java
@@ -8,10 +8,54 @@ import java.util.Map;
 @Service
 public class CpvServiceImpl implements CpvService {
 
-    private static final Map<String, String> SUBTYPE_TO_CPV = Map.of(
-            "WASTE_MANAGEMENT", "90510000",
-            "GRAVEL_ROAD", "45233000",
-            "LIGHTING_FAILURES", "50230000"
+    private static final Map<String, String> SUBTYPE_TO_CPV = Map.ofEntries(
+
+            //ENVIRONMENT
+            Map.entry("WASTE_MANAGEMENT", "90510000"),
+            Map.entry("CEMETERY_MAINTENANCE", "98370000"),
+            Map.entry("GREEN_AREA_MAINTENANCE", "77310000"),
+            Map.entry("PLAYGROUND_MAINTENANCE", "37535000"),
+            Map.entry("TERRITORY_CLEANING", "90600000"),
+            Map.entry("SPORT_FIELD_MAINTENANCE", "45212200"),
+
+            //SURFACE_REPAIR
+            Map.entry("GRAVEL_ROAD", "45233000"),
+            Map.entry("STAIRCASE", "45210000"),
+            Map.entry("SIDEWALK_REPAIR", "45233253"),
+            Map.entry("ROAD_REPAIR", "45233141"),
+            Map.entry("YARD_SURFACE_REPAIR", "45230000"),
+            Map.entry("POTHOLES_WELLS_COLLAPSE", "45233100"),
+            Map.entry("BIKE_PATH_REPAIR", "45233162"),
+            Map.entry("BRIDGE_TUNNEL_MAINTENANCE", "45221000"),
+
+            //MALFUNCTION
+            Map.entry("LIGHTING_FAILURES", "50230000"),
+
+            // ======================
+            // BUILDINGS
+            // ======================
+
+            Map.entry("BUILDING_ADMINISTRATION", "70300000"),
+            Map.entry("BUILDING_MAINTENANCE", "50700000"),
+
+            // ======================
+            // SEASONAL_ISSUES
+            // ======================
+
+            Map.entry("SIDEWALK_CLEANING", "90610000"),
+            Map.entry("BUS_STOP_CLEANING", "90610000"),
+            Map.entry("YARD_CLEANING", "90610000"),
+            Map.entry("BIKE_PATH_CLEANING", "90610000"),
+            Map.entry("PARKING_LOT_CLEANING", "90610000"),
+            Map.entry("STREET_CLEANING", "90610000"),
+
+            // ======================
+            // TRAFFIC (EISMAS)
+            // ======================
+
+            Map.entry("STREET_LIGHTING", "50230000"),
+            Map.entry("TRAFFIC_SIGNS_AND_MARKING", "45233290"),
+            Map.entry("TRAFFIC_LIGHTS_FAILURE", "50232200")
     );
 
     @Override
